f6f45407081986e1043405d49367a901f5babeaa
Timo Haasch <timo_haasch@gmx.de>
2015-10-21 20:05:17 +0200

    Added Sample Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Product.delete_all
-Product.create! id: 1, name: "Banana", price: 0.49, active: true
-Product.create! id: 2, name: "Apple", price: 0.29, active: true
-Product.create! id: 3, name: "Carton of Strawberries", price: 1.99, active: true
+Product.create! id: 1, name: "Bacon & Cheeseburger", price: 3.90, active: true
+Product.create! id: 2, name: "Chicken Spicy Wrap", price: 4.50, active: true
+Product.create! id: 3, name: "Dealer Star Pizza", price: 8.00, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
